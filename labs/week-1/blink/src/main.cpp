951f77a58bcb1774c6da0d1bf1ba4a5abcabcb13
N8BWert <n8.wert.b@gmail.com>
2025-09-14 14:11:39 -0400

    Add week 1 lab training material

---
+++ labs/week-1/blink/src/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * Blink - RoboJackets Firmware Training Week 1
+ * 
+ * As an introduction to firmware, we're going to be blinking the onboard LED on
+ * the Arduino Nano board.  The onboard LED is attached to pin 13.
+ * 
+ * As a first task, try blinking the led at 500 millisecond intervals.
+ * 
+ * Once you can blink the led every 500 milliseconds, is there a way that we can
+ * store the delay in a variable so we can change the delay between blinking the led
+ * without modifying each delay call?
+ * 
+ * Challenge: For an extra challenge, the schematics for the training board are located
+ * within the board directory of this repo. On the training board there are 5 leds that
+ * you can also control.  Try making these leds blink in a pattern of your choice.
+ * Personally, I like making them blink one after another, but the choice is up to you.
+ * 
+ * Hint: Take a look at Arduino's documentation (https://docs.arduino.cc/language-reference/).
+ * 
+ * Author: Nathaniel Wert <n8.wert.b@gmail.com>
+ */
+
+#include <Arduino.h>
+
+#define LED_PIN 13
+
+void setup() {
+  
+}
+
+void loop() {
+
+}
