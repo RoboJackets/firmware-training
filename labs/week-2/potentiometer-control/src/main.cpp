39b9a989a21e527205e564a884bc259786323799
N8BWert <n8.wert.b@gmail.com>
2025-09-21 18:11:13 -0400

    create week 2 training labs

---
+++ labs/week-2/potentiometer-control/src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * For this lab, we're going to be reading from the potentiometer and outputting the
+ * registered analog voltage to the led so that the led is mirroring the current
+ * state of the potentiometer.  For this example, make sure that the signal pin
+ * of the potentiometer is connected to pin A5 (SCL of the IMU).
+ * 
+ * Hint: Remember analogRead and analogWrite
+ * 
+ * Author: Nathaniel Wert <n8.wert.b@gmail.com>
+ */
+
+#include <Arduino.h>
+
+void setup() {
+
+}
+
+void loop() {
+  
+}
